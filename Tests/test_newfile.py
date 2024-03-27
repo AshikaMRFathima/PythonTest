efabafd7330052cf1974559cf30f1b83c1d067b4
Ashika mrf <ashikamrf71@gmail.com>
2024-03-27 23:24:48 +0400

    test

--- Tests/test_newfile.py
+++ Tests/test_newfile.py
@@ -2,34 +2,40 @@ import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
 import PageLocators
 import TestData.UserCredentials
 from TestData.UserCredentials import UserData
 from PageLocators.LoginPageLocator import LoginPageLocator
+from Configuration.conftest import init_driver
 
 from selenium.webdriver.common.selenium_manager import SeleniumManager
 from selenium.common.exceptions import InvalidSessionIdException
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-#file name should start with test_name.py
-#pytest test_newfile.py
-#python -m pytest
-#py.test
-#pytest -v
+# file name should start with test_name.py
+# pytest test_newfile.py
+# python -m pytest
+# py.test
+# pytest -v
+
+@pytest.mark.usefixtures("init_driver")
+class BaseTest:
+    pass
 
 
-class TestSample(): #class name should start with capitalTest
+@pytest.mark.run(order=1)
+class TestSample(BaseTest):  # class name should start with capitalTest
     @pytest.fixture()
-    def test_setup(self):  #self should pass argument
+    def test_setup(self):  # self should pass argument
         global driver
         driver = webdriver.Chrome()
         driver.implicitly_wait(8)
-        #driver.maximize_window()
-        #yield  #this will call tear down method everytime
+        # driver.maximize_window()
+        # yield  #this will call tear down method everytime
 
-    def test_login(self,test_setup):
+    @pytest.mark.run(order=2)
+    def test_login(self):
         driver.get("http://staging.maqta.ae")
         title = driver.title
         print(title)
@@ -41,6 +47,7 @@ class TestSample(): #class name should start with capitalTest
         enter_password = driver.find_element(By.NAME, LoginPageLocator.PASSWORD_NAME)
         enter_password.send_keys(TestData.UserCredentials.UserData.enter_password)
 
+    @pytest.mark.run(order=3)
     def test_navigate(self):
         click_login_btn = driver.find_element(By.ID, LoginPageLocator.LOGIN_BUTTON_ID)
         click_login_btn.click()
@@ -50,11 +57,9 @@ class TestSample(): #class name should start with capitalTest
         time.sleep(3)
         (driver.find_element(By.XPATH, "//span[contains(text(),'Sea')]")
          .click())
-        driver.find_element(By.XPATH," //span[normalize-space()='Vessel Operation']").click()
+        driver.find_element(By.XPATH, " //span[normalize-space()='Vessel Operation']").click()
 
     def test_tear_down(self):
         driver.close()
         driver.quit()
         print("Test completed")
-
-
