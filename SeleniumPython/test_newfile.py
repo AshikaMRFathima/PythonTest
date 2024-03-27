6d1c6d4324ac6a768e93e5f1d9cd20b9f7704841
Ashika mrf <ashikamrf71@gmail.com>
2024-03-27 22:18:07 +0400

    commit2

--- SeleniumPython/test_newfile.py
+++ SeleniumPython/test_newfile.py
@@ -3,6 +3,9 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.selenium_manager import SeleniumManager
+from selenium.common.exceptions import InvalidSessionIdException
+from selenium.webdriver.support.wait import WebDriverWait
+
 
 #file name should start with test_name.py
 #pytest test_newfile.py
@@ -18,11 +21,7 @@ class TestSample(): #class name should start with capitalTest
         driver = webdriver.Chrome()
         driver.implicitly_wait(8)
         #driver.maximize_window()
-        yield  #this will call tear down method everytime
-        driver.close()
-        #driver.quit()
-        print("Test completed")
-
+        #yield  #this will call tear down method everytime
 
     def test_login(self,test_setup):
         driver.get("http://staging.maqta.ae")
@@ -30,27 +29,29 @@ class TestSample(): #class name should start with capitalTest
         print(title)
         x = driver.title
         assert x == "ATLP - REG"
-
         enter_username = driver.find_element(By.ID, "Username")
         enter_username.send_keys('mskuser1')
         time.sleep(3)
-
         enter_password = driver.find_element(By.NAME, "Password")
         enter_password.send_keys('hOmM$26@8&2!M^T')
 
     def test_navigate(self):
         click_login_btn = driver.find_element(By.ID, "step-btn")
         click_login_btn.click()
-        time.sleep(5)
-
+        time.sleep(3)
         click_on_menu = driver.find_element(By.ID, "burger-menu-nav")
         click_on_menu.click()
-        time.sleep(5)
-
+        time.sleep(3)
         (driver.find_element(By.XPATH, "//span[contains(text(),'Sea')]")
          .click())
-        time.sleep(4)
-
         driver.find_element(By.XPATH," //span[normalize-space()='Vessel Operation']").click()
+
+        element = driver.find_element(By.ID, "burger-menu-nav")
+        assert element.text == 'Example Domains'
+
+    def test_tear_down(self):
+        driver.close()
+        driver.quit()
+        print("Test completed")
 
 
